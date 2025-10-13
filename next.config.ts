e267648de6236c76edf0621e8431d79fd9ab7f44
sharpwit <khosravi.webmaster@gmail.com>
2025-10-13 14:42:09 +0330

    feat(styles): integrate SCSS into project setup

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,14 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+  sassOptions: {
+    includePaths: ["./src/styles"],
+    prependData: `
+      @import "abstracts/variables";
+      @import "abstracts/mixins";
+      @import "abstracts/functions";
+    `,
+  },
   /* config options here */
 };
 
