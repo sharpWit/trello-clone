db3db9560104b601e44c7f574c157221de418638
sharpwit <khosravi.webmaster@gmail.com>
2025-10-14 22:50:29 +0330

    feat: add form layout

---
+++ src/components/molecules/form/form.tsx
@@ -0,0 +1,19 @@
+import { CloseIcon } from "@/shared";
+import styles from "./form.module.scss";
+import { Button, InputField } from "@/components/atoms";
+
+const FormComponent = () => {
+  return (
+    <form action="" method="post" className={styles.formElement}>
+      <InputField placeholder="Add board title" id="board" name="board" />
+      <div className={styles.actionContainer}>
+        <Button>Create board</Button>
+        <button className={styles.closeButton}>
+          <CloseIcon />
+        </button>
+      </div>
+    </form>
+  );
+};
+
+export default FormComponent;
--- src/features/board/components/board-card-generator/board-card-generator.tsx
+++ src/features/board/components/board-card-generator/board-card-generator.tsx
@@ -1,11 +1,10 @@
-import { Button, InputField } from "@/components/atoms";
 import styles from "./board-card-generator.module.scss";
+import { FormComponent } from "@/components/molecules";
 
 export const BoardCardGenerator = () => {
   return (
     <div className={styles.container}>
-      <InputField placeholder="Add board title" />
-      <Button>Create Board</Button>
+      <FormComponent />
     </div>
   );
 };
