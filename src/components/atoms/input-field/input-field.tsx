6e728745500c41afdbdbfdaf9d79f245fea0b98a
sharpwit <khosravi.webmaster@gmail.com>
2025-10-14 05:58:20 +0330

    feat: add input

---
+++ src/components/atoms/input-field/input-field.tsx
@@ -0,0 +1,53 @@
+import styles from "./input-field.module.scss";
+
+interface InputFieldProps {
+  type?: "text" | "email" | "password" | "number" | "tel";
+  placeholder?: string;
+  value?: string;
+  defaultValue?: string;
+  onChange?: (event: React.ChangeEvent<HTMLInputElement>) => void;
+  onFocus?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  onBlur?: (event: React.FocusEvent<HTMLInputElement>) => void;
+  disabled?: boolean;
+  readOnly?: boolean;
+  required?: boolean;
+  name?: string;
+  id?: string;
+  className?: string;
+}
+
+const InputField = ({
+  type = "text",
+  placeholder,
+  value,
+  defaultValue,
+  onChange,
+  onFocus,
+  onBlur,
+  disabled = false,
+  readOnly = false,
+  required = false,
+  name,
+  id,
+  className = "",
+}: InputFieldProps) => {
+  return (
+    <input
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      defaultValue={defaultValue}
+      onChange={onChange}
+      onFocus={onFocus}
+      onBlur={onBlur}
+      disabled={disabled}
+      readOnly={readOnly}
+      required={required}
+      name={name}
+      id={id}
+      className={`${styles.inputElement} ${className}`}
+    />
+  );
+};
+
+export default InputField;
--- src/features/board/components/board-card-generator/board-card-generator.tsx
+++ src/features/board/components/board-card-generator/board-card-generator.tsx
@@ -1,7 +1,13 @@
+import { Button, InputField } from "@/components/atoms";
 import styles from "./board-card-generator.module.scss";
 
 export const BoardCardGenerator = () => {
-  return <div className={styles.container}>BoardCardGenerator</div>;
+  return (
+    <div className={styles.container}>
+      <InputField placeholder="Add board title" />
+      <Button>Create Board</Button>
+    </div>
+  );
 };
 
 export default BoardCardGenerator;
