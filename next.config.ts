14d7986220873d58a0196f6a91818a245cbd3a8c
sharpwit <khosravi.webmaster@gmail.com>
2025-10-17 23:16:12 +0330

    fix: building static pages

--- next.config.ts
+++ next.config.ts
@@ -3,22 +3,13 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   sassOptions: {
-<<<<<<< HEAD
-    includePaths: ["./src/styles"],
-    prependData: `
-      @import "abstracts/variables";
-      @import "abstracts/mixins";
-      @import "abstracts/functions";
-=======
     includePaths: [path.join(__dirname, "src/styles")],
-    additionalData: `
+    prependData: `
       @use "abstracts/variables" as *;
       @use "abstracts/mixins" as *;
       @use "abstracts/functions" as *;
->>>>>>> 0ae7cfd (fix: update some type bugs)
     `,
   },
-  /* config options here */
 };
 
 export default nextConfig;
--- src/app/(boards)/boards/[boardId]/page.tsx
+++ src/app/(boards)/boards/[boardId]/page.tsx
@@ -1,5 +1,10 @@
 import { ListPageWrapper } from "@/features";
 
+// Return empty array and handle dynamic boards at runtime
+export const generateStaticParams = async () => {
+  return [];
+};
+
 export default async function BoardPage({
   params,
 }: Readonly<{
